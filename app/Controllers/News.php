5dd40569f71b5e41539b79f0862e7957bcd21e84
TadeuGT <gabrieltadeugeromel@gmail.com>
2022-05-22 23:41:06 -0300

    First commit: envio

---
+++ app/Controllers/News.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\NewsModel;
+
+class News extends BaseController
+{
+    public function index()
+    {
+        $model = model(NewsModel::class);
+
+        $data = [
+            'news'  => $model->getNews(),
+            'title' => 'News archive',
+        ];
+
+        echo view('templates/header', $data);
+        echo view('news/overview', $data);
+        echo view('templates/footer', $data);
+    }
+
+    public function view($slug = null)
+    {
+        $model = model(NewsModel::class);
+
+        $data['news'] = $model->getNews($slug);
+
+        if (empty($data['news'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        echo view('templates/header', $data);
+        echo view('news/view', $data);
+        echo view('templates/footer', $data);
+    }
+
+}
